5d2e01f693cb25f03e0af1de005e38de2f9bf94d
Haiko Hertes <github@hertes.net>
2020-06-02 19:39:18 +0200

    Some more comments

--- DeployARMtoAzure.ps1
+++ DeployARMtoAzure.ps1
@@ -1,10 +1,12 @@
 ﻿# Find all template files in naming scheme XYZ.json
 # For each template file, a parameters file with naming format XYZ.parameters.json is expected
 $templates = Get-ChildItem -Path "arm\" -Filter "*.json" -File -Recurse | Where Name -notlike "*parameters.json"
+Write-Host "Found $($templates.Count) JSON templates..."
 
 # Iterate through all Templates, ordered by filename
 ForEach($template in ($templates | Sort-Object -Property BaseName))
 {   
+    Write-Host "Using $($template.Name)..."
     # Create RG if not allready existing
     $RGName = Split-Path $template.DirectoryName -Leaf
     $Location = Get-Content -Path "$($template.DirectoryName)\location.txt"
